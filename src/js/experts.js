0084a5183a5e1e1e36f9014a1324295176a8d8d7
m1xture <109725502+m1xture@users.noreply.github.com>
2023-11-04 19:54:19 +0200

    rtdryh

---
+++ src/js/experts.js
@@ -0,0 +1,140 @@
+const scientists = [
+  {
+    name: "Albert",
+    surname: "Einstein",
+    url: "../img/albert.jpg",
+    alt: "Einstein",
+    born: 1879,
+    dead: 1955,
+    id: 1,
+  },
+  {
+    name: "Isaac",
+    surname: "Newton",
+    url: "../img/newton.webp",
+    alt: "Isaac Newton",
+    born: 1643,
+    dead: 1727,
+    id: 2,
+  },
+  {
+    name: "Galileo",
+    surname: "Galilei",
+    url: "../img/galileo.jpg",
+    alt: "Galileo",
+    born: 1564,
+    dead: 1642,
+    id: 3,
+  },
+  {
+    name: "Marie",
+    surname: "Curie",
+    url: "../img/curie.jpg",
+    alt: "Marie Curie",
+    born: 1867,
+    dead: 1934,
+    id: 4,
+  },
+  {
+    name: "Johannes",
+    surname: "Kepler",
+    url: "",
+    born: 1571,
+    dead: 1630,
+    id: 5,
+  },
+  {
+    name: "Nicolaus",
+    surname: "Copernicus",
+    url: "",
+    born: 1473,
+    dead: 1543,
+    id: 6,
+  },
+  {
+    name: "Max",
+    surname: "Planck",
+    url: "",
+    born: 1858,
+    dead: 1947,
+    id: 7,
+  },
+  {
+    name: "Katherine",
+    surname: "Blodgett",
+    url: "",
+    born: 1898,
+    dead: 1979,
+    id: 8,
+  },
+  {
+    name: "Ada",
+    surname: "Lovelace",
+    url: "",
+    born: 1815,
+    dead: 1852,
+    id: 9,
+  },
+  {
+    name: "Sarah E.",
+    surname: "Goode",
+    url: "",
+    born: 1855,
+    dead: 1905,
+    id: 10,
+  },
+  {
+    name: "Lise",
+    surname: "Meitner",
+    url: "",
+    born: 1878,
+    dead: 1968,
+    id: 11,
+  },
+  {
+    name: "Hanna",
+    surname: "Hammarström",
+    url: "",
+    born: 1829,
+    dead: 1909,
+    id: 12,
+  },
+];
+const refs = {
+  galleryEl: document.querySelector("[data-experts]"),
+  galleryElems: document.querySelectorAll(".gallery__item"),
+  btnsListEl: document.querySelector("[data-experts-controls]"),
+  controlsEl: document.querySelectorAll("[data-experts-btn]"),
+};
+const { galleryEl, controlsEl, btnsListEl, galleryElems } = refs;
+
+//# Btns functions begin
+
+function render(arrayOfScientists) {
+  console.log("click");
+  galleryEl.innerHTML = "";
+  arrayOfScientists.forEach((scientist) => {
+    galleryEl.innerHTML += `<li class="gallery__item"><img src="${scientist.url}" alt="${scientist}" /><div class="gallery-box"><h3>${scientist.name} ${scientist.surname}</h3><p>${scientist.born} - ${scientist.dead}</p></div></li>`;
+  });
+}
+
+function findExpertsBornedIn() {
+  const resOfFlter = scientists.filter(
+    (scientist) => scientist.born >= 1801 && scientist.born <= 1900
+  );
+  render(resOfFlter);
+}
+
+//# Btns functions end
+
+btnsListEl.addEventListener("click", (e) => {
+  const indexOfClikedElem = Array.from(controlsEl).findIndex(
+    (btn) => btn === e.target
+  );
+
+  if (indexOfClikedElem !== -1) {
+    if (indexOfClikedElem === 0) {
+      findExpertsBornedIn();
+    }
+  }
+});
