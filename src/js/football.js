ddc8aa90f35a62fd6b9a6c17350a15cd851f2466
JakeDer29 <108018683+Yakov29@users.noreply.github.com>
2023-11-07 23:15:26 +0200

    football

--- src/js/football.js
+++ src/js/football.js
@@ -1,18 +1,43 @@
 const ball = document.querySelector('.ball');
 const footballBlock = document.querySelector('.football-block');
 
+const ballWidth = ball.offsetWidth;
+const ballHeight = ball.offsetHeight;
+const ballHalfWidth = ballWidth / 2;
+const ballHalfHeight = ballHeight / 2;
+
+let ballX = ball.offsetLeft + ballHalfWidth;
+let ballY = ball.offsetTop + ballHalfHeight;
+
+const force = 100; // Фиксированная сила отталкивания
+
 footballBlock.addEventListener('mousemove', (event) => {
-  const ballWidth = ball.offsetWidth;
-  const ballHeight = ball.offsetHeight;
-  const offsetX = ballWidth / 2;
-  const offsetY = ballHeight / 2;
-  let x = event.clientX - footballBlock.getBoundingClientRect().left - offsetX;
-  let y = event.clientY - footballBlock.getBoundingClientRect().top - offsetY;
+  const mouseX = event.clientX - footballBlock.getBoundingClientRect().left;
+  const mouseY = event.clientY - footballBlock.getBoundingClientRect().top;
 
-  // Ограничиваем координаты мяча, чтобы он не выходил за пределы дива
-  x = Math.min(Math.max(x, 0), footballBlock.clientWidth - ballWidth);
-  y = Math.min(Math.max(y, 0), footballBlock.clientHeight - ballHeight);
+  const deltaX = mouseX - ballX;
+  const deltaY = mouseY - ballY;
 
-  ball.style.left = x + 'px';
-  ball.style.top = y + 'px';
+  const distance = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+
+  if (distance < ballHalfWidth) {
+    const angle = Math.atan2(deltaY, deltaX);
+
+    const newBallX = ballX - force * Math.cos(angle);
+    const newBallY = ballY - force * Math.sin(angle);
+
+    const maxWidth = footballBlock.clientWidth - ballWidth;
+    const maxHeight = footballBlock.clientHeight - ballHeight;
+    const x = Math.min(Math.max(newBallX - ballHalfWidth, 0), maxWidth);
+    const y = Math.min(Math.max(newBallY - ballHalfHeight, 0), maxHeight);
+
+    const dx = (x - ballX) * 0.05;
+    const dy = (y - ballY) * 0.05;
+
+    ballX += dx;
+    ballY += dy;
+
+    ball.style.left = ballX - ballHalfWidth + 'px';
+    ball.style.top = ballY - ballHalfHeight + 'px';
+  }
 });
